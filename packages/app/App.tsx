6b5c6ebf0121c9a3026425219356e95e6cdcd298
Marcuz Gabriel Larsen <agl@topdanmark.dk>
2024-10-15 20:57:04 +0200

    chore: Update app folder to support latest React Native

---
+++ packages/app/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import ConfettiExample from 'react-native-skia-confetti';
+
+function App(): React.JSX.Element {
+  return <ConfettiExample />;
+}
+
+export default App;
--- packages/app/index.js
+++ packages/app/index.js
@@ -2,12 +2,8 @@
  * @format
  */
 
-import { AppRegistry, LogBox } from 'react-native';
-import ConfettiExample from '../npm/src/example';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-/* NOTE: Require cycle is caused by worklets/index.ts and hooks/index.ts.
-It is not a problem so we ignore it. */
-LogBox.ignoreLogs(['Require cycle:']);
-
-AppRegistry.registerComponent(appName, () => ConfettiExample);
+AppRegistry.registerComponent(appName, () => App);
